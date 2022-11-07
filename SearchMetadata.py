2a04f9624dd34464426934e291acab6f3b263b0c
Joel Tapia Salvador <91463596+NIU1638962@users.noreply.github.com>
2022-11-07 23:05:59 +0100

    Update 20221107a.

--- SearchMetadata.py
+++ SearchMetadata.py
@@ -9,24 +9,24 @@ class SearchMetadata:
     def __init__(self, music_data):
         self._music_data = music_data
 
-    def title(self, sub: str) -> list[str]:
+    def title(self, sub: str) -> list:
         return [i for i in self.music_data if sub in self._music_data.get_title(i)]
 
-    def artist(self, sub: str) -> list[str]:
+    def artist(self, sub: str) -> list:
         return [i for i in self.music_data if sub in self._music_data.get_artist(i)]
 
-    def album(self, sub: str) -> list[str]:
+    def album(self, sub: str) -> list:
         return [i for i in self.music_data if sub in self._music_data.get_album(i)]
 
-    def genre(self, sub: str) -> list[str]:
+    def genre(self, sub: str) -> list:
         return [i for i in self.music_data if sub in self._music_data.get_genre(i)]
 
-    def and_operator(self, list1: list[str], list2: list[str]) -> list[str]:
+    def and_operator(self, list1: list, list2: list) -> list:
         t1 = {list1}
         t2 = {list2}
         return list(t1.intersection(t2))
 
-    def or_operator(self, list1: list[str], list2: list[str]) -> list[str]:
+    def or_operator(self, list1: list, list2: list) -> list:
         t1 = {list1}
         t2 = {list2}
         return list(t1.union(t2))
