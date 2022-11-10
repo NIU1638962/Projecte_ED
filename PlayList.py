42069eb098755c23a6599e7b3e0fcee0e0b7a9ce
Carlos <58601077+carlosgrtz@users.noreply.github.com>
2022-11-10 15:29:59 +0100

    Update PlayList.py

--- PlayList.py
+++ PlayList.py
@@ -1,9 +1,57 @@
-# -*- coding: utf-8 -*-
-"""
-p1_main.py : ** REQUIRED ** El vostre codi de la classe PlayList.
-"""
 import cfg
-
-
+import uuid
+#Func5
 class PlayList:
-    pass
+    def __init__(self, musicID, musicPlayer):
+        self.__playlist = []
+        self.__musicID = musicID
+        self.__musicPlayer = musicPlayer
+    @property
+    def playlist(self):
+        return self.__playlist
+    
+    @playlist.setter
+    def playlist(self, value):
+        self.__playlist = value
+        
+    @property
+    def musicID(self):
+        return self.__musicID
+    
+    @property
+    def musicPlayer(self):
+        return self.__musicPlayer    
+    
+    def load_file(self, file: str):
+        self.playlist = []
+        
+        try:
+            fitxer = open(file,"r")
+        except:
+            raise FileNotFoundError
+        
+        for line in fitxer:
+            if line[0]!="#" and line[-5].strip()==".mp3":
+                uuid_get = uuid.uuid5(uuid.NAMESPACE_URL,line.strip())
+                if uuid_get in self.musicID.uuid_dict:
+                        if uuid_get not in self.playlist:                                                                                                                                                                                                                                                                                                                                                                                                   
+                            self.playlist.append(uuid_get)
+        fitxer.close()
+        
+    def play(self,mode): 
+        for uuid_aux in self.playlist:
+            self.musicPlayer.play_song(uuid_aux, mode) 
+        
+#Func7 
+    def add_song_at_end(self,uuid: str):
+        self.playlist.append(uuid)
+    def remove_first_song(self): 
+        try:
+            self.playlist.pop(0)
+        except:
+            return None
+    def remove_last_song(self): 
+        try:
+            self.llista_repr.pop(-1)
+        except:
+            return None
