6496b520b08260ab3cae38f37283a762b5c69629
Joel Tapia Salvador <91463596+NIU1638962@users.noreply.github.com>
2022-11-28 11:21:52 +0100

    Arxius Nous

--- GrafHash.py
+++ GrafHash.py
@@ -2,3 +2,24 @@
 """
 GrafHash.py : ** REQUIRED ** El vostre codi de la classe GrafHash.
 """
+from ElementData import ElementData
+
+
+class GrafHash:
+    def __init__(self) -> None:
+        pass
+
+    def insert_vertex(self,key, e: ElementData) -> None:
+        pass
+
+    def get(self, key) -> ElementData:
+        pass 
+
+    def __contains__(self, key):
+        pass 
+
+    def __getitem__(self, key):
+        pass 
+
+    def __delitem__(self, key):
+        pass
--- MusicData.py
+++ MusicData.py
@@ -1,6 +1,9 @@
+from typing import Iterator, Tuple
+import uuid
 import eyed3
 from cfg import ROOT_DIR
 import os
+from PlayList import PlayList
 
 
 # Func3
@@ -90,3 +93,24 @@ class MusicData:
 
     def existent(self, file: str) -> bool:
         return file in [v[0] for v in self.__songs.items()]
+
+    def read_playlist(self, obj_playlist: PlayList):
+        pass
+
+    def get_song_rank(seld, uuid: str) -> int:
+        pass
+
+    def get_next_songs(self, uuid: str) -> Iterator[Tuple[str, float]]:
+        pass
+
+    def get_previous_songs(self, uuid: str) -> Iterator[Tuple[str, float]]:
+        pass
+
+    def get_song_distance(self, uuid1:str, uuid2:str) -> Iterator[Tuple[int, float]]:
+        pass
+
+    def get_similar(self, uuid: str, max_list:int) -> list:
+        pass
+
+    def get_topfive(self) -> list:
+        pass
