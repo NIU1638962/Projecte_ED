d0fb96b2c4ac28d5a56000a0911c3986d1fd5182
Joel Tapia Salvador <91463596+NIU1638962@users.noreply.github.com>
2022-11-28 10:29:44 +0100

    Arxius Nous segona part.

---
+++ GrafHash.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+"""
+GrafHash.py : ** REQUIRED ** El vostre codi de la classe GrafHash.
+"""
---
+++ main_mini.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+"""
+main_mini.py : ** OPTIONAL ** Si heu fet un main vostre, poseu aquí el codi.
+                              Aquest arxiu no s'avalua automàticament.
+"""
